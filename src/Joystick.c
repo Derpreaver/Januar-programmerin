a335dc8e333a2236c3102b980dcc88ad88a36e2f
Derpreaver <46708673+Derpreaver@users.noreply.github.com>
2019-01-16 10:10:56 +0100

    Add files via upload

---
+++ src/Joystick.c
@@ -0,0 +1,67 @@
+#include "Joystick.h"
+
+
+void Init_Joystick()
+{
+	RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A
+	RCC->AHBENR |= RCC_AHBPeriph_GPIOB; // Enable clock for GPIO Port B
+	RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
+
+	// Set pin PA4 to input, joystick up
+	GPIOA->MODER &= ~(0x00000003 << (4 * 2)); // Clear mode register
+	GPIOA->MODER |= (0x00000000 << (4 * 2)); // Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+	GPIOA->PUPDR &= ~(0x00000003 << (4 * 2)); // Clear push/pull register
+	GPIOA->PUPDR |= (0x00000002 << (4 * 2)); // Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
+
+	// Set pin PB0 to input, joystick down
+	GPIOB->MODER &= ~(0x00000003 << (0 * 2)); // Clear mode register
+	GPIOB->MODER |= (0x00000000 << (0 * 2)); // Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+	GPIOB->PUPDR &= ~(0x00000003 << (0 * 2)); // Clear push/pull register
+	GPIOB->PUPDR |= (0x00000002 << (0 * 2)); // Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
+
+	// Set pin PB5 to input, joystick center
+	GPIOB->MODER &= ~(0x00000003 << (5 * 2)); // Clear mode register
+	GPIOB->MODER |= (0x00000000 << (5 * 2)); // Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+	GPIOB->PUPDR &= ~(0x00000003 << (5 * 2)); // Clear push/pull register
+	GPIOB->PUPDR |= (0x00000002 << (5 * 2)); // Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
+
+	// Set pin PC0 to input, joystick right
+	GPIOC->MODER &= ~(0x00000003 << (0 * 2)); // Clear mode register
+	GPIOC->MODER |= (0x00000000 << (0 * 2)); // Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+	GPIOC->PUPDR &= ~(0x00000003 << (0 * 2)); // Clear push/pull register
+	GPIOC->PUPDR |= (0x00000002 << (0 * 2)); // Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
+
+	// Set pin PC1 to input, joystick left
+	GPIOC->MODER &= ~(0x00000003 << (1 * 2)); // Clear mode register
+	GPIOC->MODER |= (0x00000000 << (1 * 2)); // Set mode register (0x00 – Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
+	GPIOC->PUPDR &= ~(0x00000003 << (1 * 2)); // Clear push/pull register
+	GPIOC->PUPDR |= (0x00000002 << (1 * 2)); // Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
+}
+
+
+int Update_Joystick_Input()
+{
+        //Setting the Button PINS to read
+	uint16_t Up = GPIOA->IDR & (0x0001 << 4); // Read from PA4
+	uint16_t Center = GPIOB->IDR & (0x0001 << 5); // Read from PB5
+	uint16_t Down = GPIOB->IDR & (0x0001 << 0); // Read from PB0
+	uint16_t Right = GPIOC->IDR & (0x0001 << 0); // Read from PC0
+	uint16_t Left = GPIOC->IDR & (0x0001 << 1); // Read from PC1
+	if(Up != 0 || Down != 0 || Center != 0 || Right != 0 || Left != 0)
+		{
+		if(Up != 0)
+			{return 1;}
+		if(Center != 0)
+			{return 2;}
+		if(Right != 0)
+			{return 3;}
+		if(Down != 0)
+			{return 4;}
+		if(Left != 0)
+			{return 5;}
+		}
+		else
+			{return 0;}
+}
+
+
