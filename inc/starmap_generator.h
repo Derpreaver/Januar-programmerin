1115cf66fab8d3b11e98eeae5173932c15fbe7c2
philipklok1 <46707764+philipklok1@users.noreply.github.com>
2019-01-18 13:45:10 +0100

    Update starmap_generator.h

--- inc/starmap_generator.h
+++ inc/starmap_generator.h
@@ -4,9 +4,9 @@
 #include "30010_io.h"
 #include "stm32f30x_conf.h"
 
-//in beginning of main write char starmap[30][75]
-//when calling the function write stargen(level,starmap), where level is an integer and starmap calls to the initialization of starmap earlier in the script
-
+//In beginning of main write char starmap[30][75]
+//When calling the function write stargen(level,starmap), where level is an integer and starmap calls to the initialization of starmap earlier in the script
+//If the user wishes to change dimensions of the map generated, they have to change the number throughout starmap_generator.c, starmap_generator.h and main.c
 
 
 void stargen(int level, char starmap[30][75]);
